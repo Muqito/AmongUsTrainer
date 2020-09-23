4ce1b17801d92047aebbda92b68fdc79fec5ee84
Afel <prteza@gmail.com>
2020-09-23 16:47:52 -0700

    Made speed reset to the original speed after untoggled not to arbitrary default value

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ mod hack_util;
 
 use std::str;
 
-const names: [&str; 10] = [
+const NAMES: [&str; 10] = [
     "Panic disable all hax",
     "other", //1,8,9 either don't work or arent implemented
     "Speed",
@@ -17,10 +17,10 @@ const names: [&str; 10] = [
 
 fn main() {
     let process = hack_util::attach("Among Us.exe");
-    let module = hack_util::Module::get_module("Among Us.exe", "Among Us.exe");
+    let _module = hack_util::Module::get_module("Among Us.exe", "Among Us.exe");
 
-    //let player_control_ptr = process.pointer_from_offsets(0, vec![0x785A0000, 0x5C, 0x0]);
-    //let game_options_data_ptr = process.pointer_from_offsets(0, vec![0x785A0000, 0x5C, 0x4]);
+    //let player_control_ptr = process.pointer_from_offsets(0, vec![0x5d2a0000, 0x5C, 0x0]);
+    //let game_options_data_ptr = process.pointer_from_offsets(0, vec![0x5d2a0000, 0x5C, 0x4]);
 
     let mut was_done: [bool; 10] = [false; 10];
     let mut ogspeed: f32 = 1.0;
@@ -43,6 +43,10 @@ fn main() {
                     match x {
                         0 => {
                             speed = false;
+                            let speed_ptr = process
+                                .pointer_from_offsets(0x5d2a0000, vec![0xDA3C30, 0x5C, 0x14])
+                                + 0x14;
+                            process.write_memory::<f32>(speed_ptr, ogspeed);
                             cooldown = false;
                             range = false;
                             vent = false;
@@ -53,7 +57,18 @@ fn main() {
                         //Closet speed hack (1.2x)
                         1 => other = !other,
                         //Blatant speed hack (2x)
-                        2 => speed = !speed,
+                        2 => {
+                            let speed_ptr = process
+                                .pointer_from_offsets(0x5d2a0000, vec![0xDA3C30, 0x5C, 0x14])
+                                + 0x14;
+                            if !speed {
+                                ogspeed = process.read_memory::<f32>(speed_ptr);
+                                process.write_memory::<f32>(speed_ptr, 4.0)
+                            } else {
+                                process.write_memory::<f32>(speed_ptr, ogspeed)
+                            }
+                            speed = !speed;
+                        }
                         //No Kill Cooldown
                         3 => cooldown = !cooldown,
                         //Long Kill distance
@@ -71,7 +86,7 @@ fn main() {
                         _ => {}
                     }
                     if x != 0 && x != 1 && x != 8 && x != 9 {
-                        println!("Toggled mod [{}] {}", x, names[x as usize]);
+                        println!("Toggled mod [{}] {}", x, NAMES[x as usize]);
                     }
                 }
             } else {
@@ -80,24 +95,14 @@ fn main() {
         }
         //Passive Hacks
 
-        if other {}
-
-        let speed_ptr = process.pointer_from_offsets(0x785A0000, vec![0xDA3C30, 0x5C, 0x14]) + 0x14;
-        if speed {
-            ogspeed = process.read_memory::<f32>(speed_ptr);
-            process.write_memory::<f32>(speed_ptr, 3.0);
-        } else {
-            process.write_memory::<f32>(speed_ptr, ogspeed);
-        }
-
         if cooldown {
             let kill_cooldown_ptr =
-                process.pointer_from_offsets(0x785A0000, vec![0xDA5A84, 0x5C, 0x0]) + 0x44;
+                process.pointer_from_offsets(0x5d2a0000, vec![0xDA5A84, 0x5C, 0x0]) + 0x44;
             process.write_memory::<f32>(kill_cooldown_ptr, 0.0);
         }
 
         let kill_range_ptr =
-            process.pointer_from_offsets(0x785A0000, vec![0xDA5A84, 0x5C, 0x4]) + 0x40;
+            process.pointer_from_offsets(0x5d2a0000, vec![0xDA5A84, 0x5C, 0x4]) + 0x40;
         if range {
             process.write_memory::<u32>(kill_range_ptr, 2);
         } else {
@@ -106,16 +111,16 @@ fn main() {
 
         if vent {
             let vent_move_ptr =
-                process.pointer_from_offsets(0x785A0000, vec![0xDA5A84, 0x5C, 0x0]) + 0x30;
+                process.pointer_from_offsets(0x5d2a0000, vec![0xDA5A84, 0x5C, 0x0]) + 0x30;
             let in_vent_ptr =
-                process.pointer_from_offsets(0x785A0000, vec![0xDA5A84, 0x5C, 0x0]) + 0x31;
+                process.pointer_from_offsets(0x5d2a0000, vec![0xDA5A84, 0x5C, 0x0]) + 0x31;
             if process.read_memory::<u8>(in_vent_ptr) == 1 {
                 process.write_memory::<u8>(vent_move_ptr, 2);
             }
         }
 
         let player_imposter_ptr =
-            process.pointer_from_offsets(0x785A0000, vec![0xDA5A84, 0x5C, 0x0, 0x34]) + 0x28;
+            process.pointer_from_offsets(0x5d2a0000, vec![0xDA5A84, 0x5C, 0x0, 0x34]) + 0x28;
 
         if imposter {
             process.write_memory::<u8>(player_imposter_ptr, 1);
@@ -124,7 +129,7 @@ fn main() {
         }
 
         let vision_radius_ptr =
-            process.pointer_from_offsets(0x785A0000, vec![0xDA5A84, 0x5C, 0x0, 0x54]) + 0x1C;
+            process.pointer_from_offsets(0x5d2a0000, vec![0xDA5A84, 0x5C, 0x0, 0x54]) + 0x1C;
 
         if fullbright {
             process.write_memory::<f32>(vision_radius_ptr, 20.0);
@@ -139,11 +144,11 @@ fn main() {
 fn imposters(process: &hack_util::Process) {
     for i in 1..9 {
         let player_imposter_ptr = process.pointer_from_offsets(
-            0x785A0000,
+            0x5d2a0000,
             vec![0xDA5A84, 0x5C, 0x0, 0x24, 0x8, 0x10 + (0x4 * i)],
         ) + 0x28;
         let player_name_ptr = process.pointer_from_offsets(
-            0x785A0000,
+            0x5d2a0000,
             vec![0xDA5A84, 0x5C, 0x0, 0x24, 0x8, 0x10 + (0x4 * i), 0xC],
         ) + 0xC;
 
@@ -165,11 +170,11 @@ fn dead(process: &hack_util::Process) {
 
     for i in 1..9 {
         let player_dead_ptr = process.pointer_from_offsets(
-            0x785A0000,
+            0x5d2a0000,
             vec![0xDA5A84, 0x5C, 0x0, 0x24, 0x8, 0x10 + (0x4 * i)],
         ) + 0x29;
         let player_name_ptr = process.pointer_from_offsets(
-            0x785A0000,
+            0x5d2a0000,
             vec![0xDA5A84, 0x5C, 0x0, 0x24, 0x8, 0x10 + (0x4 * i), 0xC],
         ) + 0xC;
 
@@ -193,14 +198,14 @@ fn dead(process: &hack_util::Process) {
 }
 
 fn help(a: bool, b: bool, c: bool, d: bool, e: bool, f: bool, g: bool) {
-    println!("[0] {}", names[0]);
-    println!("[1] <{}> {}", a, names[1]);
-    println!("[2] <{}> {}", b, names[2]);
-    println!("[3] <{}> {}", c, names[3]);
-    println!("[4] <{}> {}", d, names[4]);
-    println!("[5] <{}> {}", e, names[5]);
-    println!("[6] <{}> {}", f, names[6]);
-    println!("[7] <{}> {}", g, names[7]);
-    println!("[8] {}", names[8]);
-    println!("[9] {}", names[9]);
+    println!("[0] {}", NAMES[0]);
+    println!("[1] <{}> {}", a, NAMES[1]);
+    println!("[2] <{}> {}", b, NAMES[2]);
+    println!("[3] <{}> {}", c, NAMES[3]);
+    println!("[4] <{}> {}", d, NAMES[4]);
+    println!("[5] <{}> {}", e, NAMES[5]);
+    println!("[6] <{}> {}", f, NAMES[6]);
+    println!("[7] <{}> {}", g, NAMES[7]);
+    println!("[8] {}", NAMES[8]);
+    println!("[9] {}", NAMES[9]);
 }
